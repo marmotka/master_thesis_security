8ea2dce0eb8c3ccb2e94ce31282a2d5775eb0676
marmotka <dimanas@gmail.com>
2025-10-17 17:59:00 +0300

    test updates

---
+++ src/main/java/com/fmi/quarkus/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.fmi.quarkus.controller;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.SecurityContext;
+
+@Path("/api/users")
+public class UserController {
+
+    @GET
+    @RolesAllowed("user")
+    @Path("/current-user")
+    public String getCurrentUser(@Context SecurityContext securityContext) {
+        return securityContext.getUserPrincipal().getName();
+    }
+}

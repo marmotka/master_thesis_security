3e22324fee4a3e12a079fefea93880536f068030
marmotka <dimanas@gmail.com>
2025-10-17 18:05:06 +0300

    foxed annoations

--- src/main/java/com/fmi/quarkus/controller/AdminController.java
+++ src/main/java/com/fmi/quarkus/controller/AdminController.java
@@ -1,7 +1,10 @@
 package com.fmi.quarkus.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/admin")
 public class AdminController {
